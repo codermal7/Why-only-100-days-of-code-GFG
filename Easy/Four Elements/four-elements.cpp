750c334226fe40aad1ea9ac00e1873c03ac9f5f6
Anuj Kumar Pandey <83698322+codermal7@users.noreply.github.com>
2023-10-29 23:14:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Four Elements/four-elements.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool find4Numbers(int A[], int n, int X);
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, i, x;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cin>>x;
+        cout << find4Numbers(a, n, x) << endl;
+
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+bool find4Numbers(int A[], int n, int X) 
+{
+    sort(A,A+n);
+    for (int i = 0; i < n - 3; i++)
+    {
+        for (int j = i + 1; j < n - 2; j++)
+        {
+            int start = j + 1;
+            int end = n - 1;
+            int target = X - A[i] - A[j];
+            
+            while (start < end)
+            {
+                int sum = A[start] + A[end];
+                if (sum == target)
+                    return true;
+                    
+                if (sum < target)
+                    start++;
+                    
+                else
+                    end--;
+            }
+        }
+    }          
+    return false;
+}
